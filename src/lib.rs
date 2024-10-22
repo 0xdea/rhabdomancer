ed68023f116e7c3b7cd3176f9f1c10f699dc62c4
raptor <raptor@0xdeadbeef.info>
2024-10-22 18:15:21 +0200

    First minimal prototype that collects all xrefs to all enumerated functions

--- src/lib.rs
+++ src/lib.rs
@@ -42,51 +42,76 @@
 //! * TODO
 //!
 
+use idalib::func::Function;
+use idalib::idb::IDB;
+use idalib::xref::XRefQuery;
 use std::error::Error;
-use std::io;
 use std::path::Path;
 
-use idalib::idb::IDB;
+// TODO: const NAME: type = ...;
+// TODO: static NAME: type = ...;
 
-// Internal imports
-// use ...;
+/// Main program logic
+pub fn run(filepath: &Path) -> anyhow::Result<()> {
+    println!("[*] Trying to analyze binary file {}", filepath.display());
 
-// const NAME: type = ...;
-
-// static NAME: type = ...;
-
-/// Dispatch to function implementing the selected action
-pub fn run(filepath: &Path) -> Result<(), Box<dyn Error>> {
     // Check target file
     if !filepath.is_file() {
-        return Err(Box::new(io::Error::new(
+        return Err(anyhow::anyhow!(format!("{:?} is not a file", filepath)));
+
+        /*
+        Err(Box::new(io::Error::new(
             io::ErrorKind::NotFound,
             "not a file",
-        )));
+        ))); */
     }
 
+    // Open target file, run auto-analysis, and keep results
+    eprint!("[+] ");
     let idb = IDB::open_with(filepath, true)?;
 
     for (id, f) in idb.functions() {
         println!("{id} {}", f.name().unwrap());
+
+        // TODO: move logic outside and handle errors, maybe use a suitable collection if needed
+        get_xrefs(&idb, f);
+
+        /*
+        let xref = idb
+            .first_xref_to(f.start_address(), XRefQuery::ALL)
+            .map_or(0x0, |x| x.from());
+        println!("{:x}", xref)
+        */
     }
 
     Ok(())
 }
 
-// Other functions ...
+/// TODO
+fn get_xrefs(idb: &IDB, func: Function) -> anyhow::Result<()> {
+    let mut current = idb
+        .first_xref_to(func.start_address(), XRefQuery::ALL)
+        .ok_or_else(|| anyhow::anyhow!("no XREFs to function {}", func.name().unwrap()))?;
+
+    loop {
+        println!("{:#x}", current.from());
+
+        match current.next_to() {
+            Some(next) => current = next,
+            None => break,
+        }
+    }
+
+    Ok(())
+}
+
 // TODO: grab config (insecure functions, tier, maybe message from external file); either use regular file, config (or more secure alternatives), or other serialization
 // TODO: see also https://github.com/Accenture/VulFi
 // TODO: reason on the output to make it usable/perhaps importable into IDA Pro and/or other tools; perhaps we can save an IDA db that can be opened in the tool
-
-fn get_function_addr(name: &str) -> Option<usize> {
-    todo!()
-}
+// TODO: add bookmark/comment (see also ghidra version), but collect also the calling function's name
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
