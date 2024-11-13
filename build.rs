5276f115b580baed78db1a6c3a821a5915aa11d9
raptor <raptor@0xdeadbeef.info>
2024-11-13 16:42:27 +0100

    Update build.rs for graceful fallback to link against stub libraries

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    idalib_build::configure_linkage()?;
+    let (_, ida_path, idalib_path) = idalib_build::idalib_install_paths_with(false);
+    if !ida_path.exists() || !idalib_path.exists() {
+        idalib_build::configure_idasdk_linkage();
+    } else {
+        idalib_build::configure_linkage()?;
+    }
     Ok(())
 }
