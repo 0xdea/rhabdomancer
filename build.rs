782b0821f9a5b2ca51bedeaf578f7ab68a34d903
raptor <raptor@0xdeadbeef.info>
2024-11-13 22:30:52 +0100

    Update build.rs to hopefully fix build problem with Windows

--- build.rs
+++ build.rs
@@ -2,10 +2,17 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let (_, ida_path, idalib_path) = idalib_build::idalib_install_paths_with(false);
-    if !ida_path.exists() || !idalib_path.exists() {
-        idalib_build::configure_idasdk_linkage();
-    } else {
+
+    if ida_path.exists() && idalib_path.exists() {
         idalib_build::configure_linkage()?;
+    } else {
+        idalib_build::configure_idasdk_linkage();
+
+        // FIXME: see `configure_linkage()` in idalib-build
+        if cfg!(target_os = "windows") {
+            println!("cargo::rustc-link-arg=/FORCE:UNRESOLVED");
+        }
     }
+
     Ok(())
 }
