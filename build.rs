eef36e4fe7d57d7df7061fe258be9b70d068bcaa
raptor <raptor@0xdeadbeef.info>
2024-12-18 14:42:18 +0100

    chore: further improve wording of build warning in case ida installation is not found

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     let (_, ida_path, idalib_path) = idalib_build::idalib_install_paths_with(false);
     if !ida_path.exists() || !idalib_path.exists() {
-        println!("cargo::warning=IDA installation not found, check your IDADIR environment variable");
+        println!("cargo::warning=IDA installation not found, check your IDADIR env var");
         idalib_build::configure_idasdk_linkage();
     } else {
         idalib_build::configure_linkage()?;
