f79c762a4c095c3fbfc1c8f0c0850aee28843b69
raptor <raptor@0xdeadbeef.info>
2025-02-25 15:23:09 +0100

    chore: improve error handling in tests

--- tests/main.rs
+++ tests/main.rs
@@ -22,7 +22,7 @@ fn main() -> anyhow::Result<()> {
     let n_marks = rhabdomancer::run(Path::new(FILENAME))?;
     println!();
     print!("[*] Checking number of marked call locations... ");
-    assert_eq!(n_marks, N_MARKS);
+    assert_eq!(n_marks, N_MARKS, "wrong number of marked call locations");
     println!("Ok.");
 
     // Open IDB and show everything to ensure search doesn't miss anything
@@ -31,30 +31,38 @@ fn main() -> anyhow::Result<()> {
 
     // Check the number of bookmarks
     print!("[*] Checking number of bookmarks... ");
-    assert_eq!(idb.bookmarks().len(), n_marks);
+    assert_eq!(idb.bookmarks().len(), n_marks, "wrong number of bookmarks");
     println!("Ok.");
 
     // Check bookmarks
     print!("[*] Checking bookmarks... ");
     for i in 0..idb.bookmarks().len() {
-        assert!(idb
-            .bookmarks()
-            .get_description_by_index(i)
-            .is_some_and(|desc| desc.starts_with("[BAD ")));
+        assert!(
+            idb.bookmarks()
+                .get_description_by_index(i)
+                .is_some_and(|desc| desc.starts_with("[BAD ")),
+            "wrong description in bookmark"
+        );
     }
     println!("Ok.");
 
     // Check the number of comments
     print!("[*] Checking number of comments... ");
-    assert_eq!(idb.find_text_iter("[BAD ").count(), n_marks as usize);
+    assert_eq!(
+        idb.find_text_iter("[BAD ").count(),
+        n_marks as usize,
+        "wrong number of comments"
+    );
     println!("Ok.");
 
     // Check comments
     print!("[*] Checking comments... ");
     for i in 0..idb.bookmarks().len() {
-        assert!(idb
-            .get_cmt(idb.bookmarks().get_address(i).unwrap())
-            .is_some_and(|desc| desc.starts_with("[BAD ")));
+        assert!(
+            idb.get_cmt(idb.bookmarks().get_address(i).unwrap())
+                .is_some_and(|desc| desc.starts_with("[BAD ")),
+            "wrong description in comment"
+        );
     }
     println!("Ok.");
 
