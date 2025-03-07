480b0f04ecb2995ef0ec4a62d7619abc824480bc
raptor <raptor@0xdeadbeef.info>
2025-03-07 12:13:34 +0100

    doc: add error section in docs for public functions

--- src/lib.rs
+++ src/lib.rs
@@ -293,7 +293,9 @@ impl<'a> BadFunctions<'a> {
 
 /// Locate calls to potentially insecure API functions in the binary file at `filepath`.
 ///
-/// Return how many call locations were marked or an error in case something goes wrong.
+/// ## Errors
+///
+/// Returns how many call locations were marked or a generic error in case something goes wrong.
 pub fn run(filepath: &Path) -> anyhow::Result<BookmarkIndex> {
     // Load known bad API function names from the configuration file
     println!("[*] Loading known bad API function names");
