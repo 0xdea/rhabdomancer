d93af63d1ce9317b760c8af4b873bfba072dbf32
raptor <raptor@0xdeadbeef.info>
2025-12-16 16:37:03 +0100

    refactor: use `args_os` instead of `args`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //! main.rs
 
 use std::env;
+use std::ffi::OsString;
 use std::path::Path;
 use std::process::ExitCode;
 
@@ -12,9 +13,12 @@ fn main() -> ExitCode {
     eprintln!("Copyright (c) 2024-2025 Marco Ivaldi <raptor@0xdeadbeef.info>");
     eprintln!();
 
+    // Force IDA Pro to stay quiet
+    idalib::force_batch_mode();
+
     // Parse command line arguments
-    let mut args = env::args();
-    let argv0 = args.next().unwrap_or_else(|| PROGRAM.to_owned());
+    let mut args = env::args_os();
+    let argv0 = args.next().unwrap_or_else(|| OsString::from(PROGRAM));
 
     let prog = Path::new(&argv0)
         .file_name()
@@ -22,12 +26,9 @@ fn main() -> ExitCode {
         .unwrap_or(PROGRAM);
 
     let filename = match (args.next(), args.next()) {
-        (Some(arg), None) if !arg.starts_with('-') => arg,
+        (Some(arg), None) if !arg.to_string_lossy().starts_with('-') => arg,
         _ => return usage(prog),
     };
-
-    // Force IDA Pro to stay quiet
-    idalib::force_batch_mode();
 
     // Let's do it
     match rhabdomancer::run(Path::new(&filename)) {
